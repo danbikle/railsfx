ce0dfc29df6c74f59ac3b8b6f56fee71a7cd5c77
Dan Bikle <danbikle@outlook.com>
2019-02-15 23:38:19 -0800

    more

--- test/system/get_tfx_data_test.rb
+++ test/system/get_tfx_data_test.rb
@@ -5,26 +5,35 @@ class GetTfxDataTest < ApplicationSystemTestCase
   test "visiting truefx.com" do
     Capybara.run_server = false
     Capybara.app_host   = 'https://truefx.com'
-    sleep 2
+    sleep 0
     visit '/?page=how'
-    sleep 2
-    visit '/?page=downloads'
-    sleep 2
+    sleep 0
     visit '/?page=partners'
-    sleep 2
+    sleep 0
     visit '/?page=about'
-    sleep 2
+    sleep 0
     visit '/?page=faqs'
-    sleep 2
+    sleep 0
     visit '/?page=contact'
-    sleep 2
+    sleep 0
     visit '/?page=privacy'
-    sleep 2
+    sleep 0
     visit '/?page=logina'
     p ENV['TRUEFX_USER']
     p ENV['TRUEFX_PASS']
-    sleep 2
-    user_elm = find("")
+    sleep 0
+    login_elm = find("#login-form")
+    user_inp  = login_elm.find('input[name="USERNAME"]')
+    pass_inp  = login_elm.find('input[name="PASSWORD"]')
+    user_inp.fill_in with: ENV['TRUEFX_USER']
+    sleep 1
+    pass_inp.fill_in with: ENV['TRUEFX_PASS']
+    b_elm = find('input[value="Login"]')
+    sleep 1
+    b_elm.click
+    sleep 4
+    visit '/?page=downloads'
+    sleep 4
     assert true
     
   end
